3fe5f5b6f6c61f47274a313c50ec156b46193ff7
jace-leee <jace.lee@planby.us>
2025-11-13 01:50:44 +0900

    fix path

--- src/components/CreateWorkspaceModal.tsx
+++ src/components/CreateWorkspaceModal.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Input } from "./Input"
-import { Button } from "./button"
+import { Button } from "./Button"
 import { X } from "lucide-react"
 
 type CreateWorkspaceModalProps = {
